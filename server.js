6077629940d834bf197e1dc42ca9aa37bb7b1dac
JeniLopez <jelopez@escuelasproa.edu.ar>
2021-11-11 20:05:58 -0300

    server changes

--- server.js
+++ server.js
@@ -2,3 +2,43 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const ejs = require("ejs");
+const app = express();
+const port = 2500;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set(express.static(path.join(__dirname, "public")));
+
+app.listen(port, () => {
+    console.log("hi! Server running on localhost:2500");
+});
+
+app.get("/", (req, res) => {
+    res.render("galeria", { productos: productos });
+});
+
+let productos = [
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+    { nombre: "manzana", tipo: "fruta", precio: "180", stock: "true" },
+];
